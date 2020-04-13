25bede9c3cebb80b2e88f404824c04b97fba3a4f
Rosemary <liaorosemary@gmail.com>
2020-04-13 13:57:44 -0400

    Made the navbar cleaner, but in this project navbar has problems with extending (when the screen becomes smaller, the links are compressed but is too hard to see)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,5 @@
 <template>
   <div>
- <!--   <el-container>
-      <el-header>
-      hello
-        <div class="my-specific-selector">
-        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" active-text-color="#ff1a1a">
-          <el-menu-item index="1"><h1><router-link to="/">Sudbury Chinese</router-link></h1></el-menu-item>
-          <el-menu-item index="2"><el-button type="text" @click="redirect('/home')">Home</el-button></el-menu-item>
-          <el-menu-item index="3"><el-button type="text" @click="redirect('/about')">About</el-button></el-menu-item>
-          <el-submenu index="4">
-            <template slot="title"><el-button type="text" @click="changeShow">Donation Projects</el-button></template>
-            <el-menu-item index="4-1" v-if="show"><el-button type="text" @click="redirect('/projects/covid19donations')">COVID-19</el-button></el-menu-item>
-            <el-menu-item index="4-2" v-if="show"><el-button type="text" @click="redirect('/projects/poor-people-donations')">Poor people of Sudbury</el-button></el-menu-item>
-          </el-submenu>
-        </el-menu>
-        </div>
-      <el-menu>
-        <el-menu-item index="index++" v-for="(value, name, index) in menuItems" :key="index">
-            <button @click="redirect(index)">{{ index }}. {{ name }}: {{ value }}</button>
-        </el-menu-item>
-        </el-menu>
-      </el-header>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
-    </el-container> -->
-
-
-
 <!--    <el-row>
         <el-col class="navbar" :class="{ 'navber-hidden': !showNavbar }" :span="24" style="position: fixed" id="nbar">
             <b-navbar style="padding: 1em">
@@ -67,21 +40,19 @@
         
             <b-collapse id="nav-collapse" is-nav>
               <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                <b-nav-item href="#">Link1</b-nav-item>
+                <b-nav-item href="#">Link2</b-nav-item>
                 <b-nav-item-dropdown text="Lang" right>
                   <b-dropdown-item href="#">EN</b-dropdown-item>
                   <b-dropdown-item href="#">ES</b-dropdown-item>
                   <b-dropdown-item href="#">RU</b-dropdown-item>
                   <b-dropdown-item href="#">FA</b-dropdown-item>
                 </b-nav-item-dropdown>
-              </b-navbar-nav>
+             </b-navbar-nav>
         
               <!-- Right aligned nav items -->
               <b-navbar-nav class="ml-auto">
-                <b-navbar-nav>
-                  <b-nav-item href="#">Chinese</b-nav-item>
-                </b-navbar-nav>
+                <b-nav-item href="#">Link2</b-nav-item>
               </b-navbar-nav>
             </b-collapse>
           </b-navbar>
