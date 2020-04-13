36c6a69876af9525545d367a83870dd4b40492aa
Rosemary <rosemary@localhost.localdomain>
2020-04-13 11:18:16 -0400

    Changed navbar, added bootstrap override, npm installed a couple of things...

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -57,6 +57,35 @@
             </b-navbar>
         </el-col>
     </el-row> -->
+        <div>
+          <b-navbar toggleable="lg" type="dark" variant="logocolor" fixed="top">
+            <b-navbar-brand href="#">
+              <img src="../assets/logoscaled.png" alt="Logo" style="width: 66%" class="d-inline-block align-top">
+            </b-navbar-brand>
+        
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        
+            <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav>
+                <b-nav-item href="#">Link</b-nav-item>
+                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                <b-nav-item-dropdown text="Lang" right>
+                  <b-dropdown-item href="#">EN</b-dropdown-item>
+                  <b-dropdown-item href="#">ES</b-dropdown-item>
+                  <b-dropdown-item href="#">RU</b-dropdown-item>
+                  <b-dropdown-item href="#">FA</b-dropdown-item>
+                </b-nav-item-dropdown>
+              </b-navbar-nav>
+        
+              <!-- Right aligned nav items -->
+              <b-navbar-nav class="ml-auto">
+                <b-navbar-nav>
+                  <b-nav-item href="#">Chinese</b-nav-item>
+                </b-navbar-nav>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-navbar>
+        </div>
   </div>
 </template>
 
