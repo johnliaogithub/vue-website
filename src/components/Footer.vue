d49170bbbaaa0e096f43b893d45bf01ac1064f59
Rosemary <liaorosemary@gmail.com>
2020-04-24 11:48:04 -0400

    Added customizable bootstrap scss, changed footer background, fixed navbar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="footer">
-    <footer id="footer">
+    <footer id="footer" class="bg-scblue">
       <div id="topf" style="height: 50px;"></div>
       <div id="innerf">
-        Author: John Liao<br />
-        Contact: <a href="mailto:johnliao5@gmail.com">johnliao5@gmail.com</a> if you have questions<br />
+        Author: John and Rosemary Liao<br />
+        Contact: <a href="mailto:johnliao5@gmail.com">johnliao5@gmail.com</a> or <a href="mailto:liaorosemary@gmail.com">liaorosemary@gmail.com</a> if you have questions<br />
         <a href="#nava">Back to top <img style="height: 10px;width: 10px;" src="https://cdn0.iconfinder.com/data/icons/arrows-11/100/arrow-1-512.png"></a><br />
       </div>
       <div class="fb-share-button" data-href="http://localhost:8080/projects/poorpeopledonations" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A8080%2Fprojects%2Fpoorpeopledonations&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
@@ -26,9 +26,15 @@ export default {
 }
 #topf {
 }
-#footer {
+#origfooter {
   color: white;
-  
+  left: 0px;
+  bottom: 0px;
+  width: 100%;
+  overflow: auto;
+}
+#origfooter {
+  color: white;
   left: 0px;
   bottom: 0px;
   width: 100%;
